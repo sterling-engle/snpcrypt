b4fce90eb52d9d559d91834df555e8a6b7b59a15
sterling-engle <sjengle@ucla.edu>
2022-01-21 20:22:18 -0800

    script to run snpcrypt --snps=-1 with each *.sam file in current working directory

---
+++ sam_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# sam_test.sh runs snpcrypt with every *.sam SAM file in the current working directory
+#
+# @author: Sterling Engle
+# @uid: 904341227
+#
+for f in *.sam ; do
+  echo "testing file:" $f ;
+  python ~/capstone/snpcrypt.py --snps -1 $f ;
+done
